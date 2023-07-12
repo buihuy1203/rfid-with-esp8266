f6f15bfd5207fe41ef2ea187b7057f1e225918af
buihuy1203 <85066488+buihuy1203@users.noreply.github.com>
2023-07-12 21:04:10 +0700

    Add files via upload

--- sketch_jun11a.ino
+++ sketch_jun11a.ino
@@ -1,19 +1,15 @@
+#include<SerialCommand.h>
 #include<Wire.h>
 #include<LiquidCrystal_I2C.h>
 #include<SPI.h>
 #include<MFRC522.h>
-/*#include<SoftwareSerial.h>
-#define Rx 2
-#define Tx 3*/
+SerialCommand sCmd;
 #define SS_PIN 10
 #define RST_PIN 9
-//SoftwareSerial mySerial(Rx, Tx);
 MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance.
-LiquidCrystal_I2C lcd(0x27,16,2);
+LiquidCrystal_I2C lcd(0X27,16,2);
 int relay = 8;
-String inputString = ""; //hold incoming data
-bool stringComplete = false; //whether the string is complete
-String defaultKey= "21 A9 B0 23";
+String defaultKey = "21 A9 B0 23";
 String openDoor = "openDoor";
 String closeDoor = "closeDoor";
 void setup()
@@ -26,62 +22,19 @@ void setup()
   mfrc522.PCD_Init(); // Initiate MFRC522
   lcd.setCursor(0,0);
   lcd.print("Move your card...");
-  inputString.reserve(200);
   Serial.begin(115200);
-  /*mySerial.begin(115200); 
-  mySerial.print(closeDoor);*/
-  Serial.print(closeDoor);
+  sCmd.addCommand("bat1", unlock);
+  sCmd.addCommand("tat1", lock);
+  sCmd.addCommand("bat2", del);
+  sCmd.addCommand("tat2", swap);
   delay(2000);
 }
 
 void loop()
-{
-  if (stringComplete){
-    if(inputString = "bat1"){
-      lcd.clear();
-  lcd.setCursor(0,0);
-    lcd.print("Door open");
-    Serial.print(openDoor);
-    digitalWrite(relay, HIGH);
-    inputString = "";
-    stringComplete = false;
-    }else if(inputString = "tat1"){
-      lcd.clear();
-  lcd.setCursor(0,0);
-      lcd.print("Door close");
-      Serial.print(closeDoor);
-      digitalWrite(relay, LOW);
-      inputString = "";
-      stringComplete = false;
-    }else if(inputString = "bat2"){
-      defaultKey = "FF FF FF FF";
-      lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Key Delete");
-    }else if(inputString = "tat2"){
-      if (!mfrc522.PICC_IsNewCardPresent())
-  {
-    return;
+{ 
+  if(Serial.available()){
+  sCmd.readSerial();
   }
-  // Select one of the cards
-  if (!mfrc522.PICC_ReadCardSerial())
-  {
-    return;
-  }
-  String newKey ="";
-  for (byte i = 0; i < mfrc522.uid.size; i++){
-  newKey.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-  newKey.concat(String(mfrc522.uid.uidByte[i], HEX));
-  }
-  defaultKey = newKey;
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Card Swapped");
-  inputString = "";
-  stringComplete = false;
-    }
-  }else{
-      // Look for new cards
   if (!mfrc522.PICC_IsNewCardPresent())
   {
     return;
@@ -98,49 +51,74 @@ void loop()
   byte letter;
   for (byte i = 0; i < mfrc522.uid.size; i++)
   {
-    lcd.setCursor(6,1);
-    lcd.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    lcd.print(mfrc522.uid.uidByte[i], HEX);
     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
     content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
   content.toUpperCase();
+  lcd.setCursor(4,1);
+  lcd.print(content);
+  delay(1000);
   if (content.substring(1) == defaultKey) //change here the UID of the card/cards that you want to give access
   {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Access Success");
     digitalWrite(relay, HIGH);
-    Serial.print(openDoor);
     delay(3000);
     digitalWrite(relay, LOW);
-    Serial.print(closeDoor);
     printMenu();
+    delay(3000);
   }
   else {
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("Access Denied");
+    Serial.print("Access Denied");
     delay(3000);
     printMenu();
-  }
-  }
-}
-void serialEvent() {
-  while (Serial.available()) {
-    // get the new byte:
-    char inChar = (char)Serial.read();
-    // add it to the inputString:
-    inputString += inChar;
-    // if the incoming character is a newline, set a flag so the main loop can
-    // do something about it:
-    if (inChar == '\n') {
-      stringComplete = true;
-    }
   }
 }
 void printMenu(){
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Move your card ...");
+}
+
+void unlock(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+    lcd.print("Door open");
+    digitalWrite(relay, HIGH);
+}
+
+void lock(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+      lcd.print("Door close");
+      digitalWrite(relay, LOW);
+}
+
+void del(){
+  defaultKey = "FF FF FF FF";
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Key Delete");
+}
+
+void swap(){
+  while( ! mfrc522.PICC_IsNewCardPresent()){
+  continue;
+  }
+  while( ! mfrc522.PICC_ReadCardSerial()){
+  continue;
+  }
+  String newKey = "";
+  for (byte i = 0; i < mfrc522.uid.size; i++){
+  newKey.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+  newKey.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  newKey.toUpperCase();
+  defaultKey = newKey.substring(1);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Card Swapped");
 }
--- sketch_jun13a.ino
+++ sketch_jun13a.ino
@@ -2,83 +2,105 @@
 #include <ESP8266mDNS.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
+#include <SoftwareSerial.h>
+SoftwareSerial s(3,1);
 ESP8266WebServer server(80);
 char htmlResponse[10000];
-int i = 0;
 const char* ssid     = "Huy's Galaxy A32";
 const char* password = "tmlw1997";
 const char openDoor_status[] PROGMEM = R"=====(<h3 style="text-align:center">Cửa đang mở</h3>)=====";
 const char closeDoor_status[] PROGMEM = R"=====(<h3 style="text-align:center">Cửa đang đóng</h3>)=====";
 const char webPage_html[] PROGMEM = R"=====(<!DOCTYPE html>
 <html>
+  <style>
+    .b{width: 120px;height: 40px;font-size: 21px;color: #FFF;background-color:#4caf50;border-radius: 10px;}
+    .t{width: 120px;height: 40px;font-size: 21px;color: #FFF;background-color:#f44336;border-radius: 10px;}
+        table.center {
+            margin-left: "auto";
+            margin-right: "auto";
+          }
+#login{ line-height: 1.5; margin: 0 auto; margin-top:150px; width:50%; font-size: 50px;background-color: white; border:2px solid black; padding:10px 10px 10px 10px; border-radius: 10px; z-index: 1000;}
+  </style>
     <head>
         <meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
         <title> Cửa tự động RFID </title>
         <meta name='viewport' content='width=device-width, initial-scale=1'>
- <style>
-    .b{width: 100px;height: 40px;font-size: 21px;color: #FFF;background-color:#4caf50;border-radius: 10px;}
-    .t{width: 100px;height: 40px;font-size: 21px;color: #FFF;background-color:#f44336;border-radius: 10px;}
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+    </head>
+    <body>
+      <div id = "login" >
+        <form id = "login-page">
+          <label for="username">ID người dùng</label><br>
+          <input style="width: 75%;height:50px;font-size: 40px" type="text" id="username" placeholder="ID người dùng"><br>
+          <label for = "inputPassword">Mật khẩu</label><br>
+          <input style="width: 75%;height:50px;font-size: 40px" type ="password" id="inputPassword" placeholder="Mật Khẩu"><br>
+          <button style="font-size: 40px" class="btn btn-lg btn-primary btn-block btn-signin" id="btnlogin" type ="submit" >Đăng nhập</button>
+        </form>    
+      </div>
+      <div id = "main" style="width: 330px;height: auto;margin: 0 auto;margin-top:150px;display: none">
+            <h1 style="text-align:center">Điều khiển cửa qua WIFI</h1>
+            <table class ="center">
+                <tr>
+                    <td><a href = '/bat1'><button id = "bat1" class='b' type="button" >Mở cửa </button></a><td>
+                    <td><a href = '/tat1'><button id = "tat1" class='t' type="button" >Đóng cửa </button></a><td>
+                    </tr>
+                <tr>
+                    <td><a href = '/bat2'><button id = "bat2" class='b' type="button" >Xóa thẻ</button></a><td>
+                    <td><a href = '/tat2'><button id = "tat2" class='t' type="button" >Đổi thẻ</button></a><td>
+                    </tr>
+            </table>
+            <h2 style="text-align:center" id = "demo">
+            </h2>
+        </div>
+        <script>
+          var myID;
+        var myPass;
+        var isLoggedIn = false;
+        $(document).ready(function() {
+          $('#btnlogin').click(function() {
+            myID = $('#username').val();
+            myPass = $('#inputPassword').val();
+            if (myID === "huynoname" && myPass === "15297583") {
+              isLoggedIn=true;
+              $('#login').remove();
+              $('#main').show();
+              return false;
+            } else {
+              alert("Sai thông tin");
+            }
+          });
+        });
+            </script>
+    </body>
+</html>)=====";
+
+const char webPageUpdate_html[] PROGMEM = R"=====(<!DOCTYPE html>
+<html>
+  <style>
+    .b{width: 120px;height: 40px;font-size: 21px;color: #FFF;background-color:#4caf50;border-radius: 10px;}
+    .t{width: 120px;height: 40px;font-size: 21px;color: #FFF;background-color:#f44336;border-radius: 10px;}
         table.center {
             margin-left: "auto";
             margin-right: "auto";
           }
   </style>
+    <head>
+        <meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
+        <title> Cửa tự động RFID </title>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
     </head>
     <body>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script type='text/javascript'>
-            function openDoor() { 
-              var open;
-              $('#bat1').click(function(e){
-                e.preventDefault();
-                open = "bat1";
-            $.get('/save?open=' + open, function(data){
-             console.log(data);
-            });
-            });
-        }
-            function closeDoor() { 
-                var close;
-              $('#tat1').click(function(e){
-                e.preventDefault();
-                close = "tat1";
-            $.get('/save?close=' + close, function(data){
-             console.log(data);
-            });
-            });
-              }
-              function deleteCard() { 
-                document.getElementById("demo").innerHTML = "Xóa thẻ thành công";
-                var del;
-              $('#bat2').click(function(e){
-                e.preventDefault();
-                del = "bat2";
-            $.get('/save?del=' + del, function(data){
-             console.log(data);
-            });
-            });
-              }
-              function swapCard(){
-                var swap;
-              $('#tat2').click(function(e){
-                e.preventDefault();
-                swap = "tat2";
-            $.get('/save?swap=' + swap, function(data){
-             console.log(data);
-            });
-            });
-              }
-            </script>
-        <div style="width: 330px;height: auto;margin: 0 auto;margin-top: 70px">
+      <div style="width: 330px;height: auto;margin: 0 auto;margin-top:150px;">
             <h1 style="text-align:center">Điều khiển cửa qua WIFI</h1>
             <table class ="center">
                 <tr>
-                    <td><button id = "bat1" class='b' type="button" onclick="openDoor()">Mở cửa </button><td>
-                    <td><button id = "tat1" class='t' type="button" onclick="closeDoor()">Đóng cửa </button><td>
+                    <td><a href = '/bat1'><button id = "bat1" class='b' type="button" >Mở cửa </button></a><td>
+                    <td><a href = '/tat1'><button id = "tat1" class='t' type="button" >Đóng cửa </button></a><td>
                     </tr>
                 <tr>
-                    <td><button id = "bat2" class='b' type="button" onclick="deleteCard()">Xóa thẻ</button><td>
-                    <td><button id = "tat2" class='t' type="button" onclick="swapCard()">Đổi thẻ</button><td>
+                    <td><a href = '/bat2'><button id = "bat2" class='b' type="button" >Xóa thẻ</button></a><td>
+                    <td><a href = '/tat2'><button id = "tat2" class='t' type="button" >Đổi thẻ</button></a><td>
                     </tr>
             </table>
             <h2 style="text-align:center" id = "demo">
@@ -90,28 +112,36 @@ void handleRoot() {
   snprintf(htmlResponse, 10000, webPage_html);
   server.send ( 200, "text/html", htmlResponse );
 }
-void handleSave() {
-  if (server.arg("open") != "") {
-    Serial.println(server.arg("open"));
-  } else if (server.arg("close") != "") {
-    Serial.println(server.arg("close"));
-  } else if (server.arg("del") != "") {
-    Serial.println(server.arg("del"));
-  } else if (server.arg("swap") != "") {
-    Serial.println(server.arg("swap"));
-  }
+void bat1(){
+  Serial.println("bat1");
+  s.print("bat1\n");
+  snprintf(htmlResponse, 10000, webPageUpdate_html);
+  server.send ( 200, "text/html", htmlResponse );
 }
-void setup(void) {
+void tat1(){
+  s.print("tat1\n");
+  Serial.println("tat1");
+  snprintf(htmlResponse, 10000, webPageUpdate_html);
+  server.send ( 200, "text/html", htmlResponse );
+}
+void bat2(){
+  s.print("bat2\n");
+  Serial.println("bat2");
+  snprintf(htmlResponse, 10000, webPageUpdate_html);
+  server.send ( 200, "text/html", htmlResponse );
+}
+void tat2(){
+  s.print("tat2\n");
+  Serial.println("tat2");
+  snprintf(htmlResponse, 10000, webPageUpdate_html);
+  server.send ( 200, "text/html", htmlResponse );
+}
+void setup() {
+  s.begin(115200);
   Serial.begin(115200);
   delay(10);
   Serial.println();
   Serial.print("Starting . . .");
-  while(i < 20){
-    Serial.println(i);
-    i=i+1;
-  }
-  i = 0;
-  Serial.println();
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
@@ -125,23 +155,16 @@ void setup(void) {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
   server.on ( "/", handleRoot );
-  server.on ("/save", handleSave);
+  server.on ("/bat1", bat1);
+  server.on ("/tat1", tat1);
+  server.on ("/bat2", bat2);
+  server.on ("/tat2", tat2);
   server.begin();
   Serial.println ( "HTTP server started" );
   delay(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   server.handleClient();
-  while (Serial.available()) {
-    String income = "";
-    income = Serial.readString();
-    if (income == "openDoor") {
-      snprintf(htmlResponse, 10000, openDoor_status);
-    }
-    else if (income == "closeDoor") {
-      snprintf(htmlResponse, 10000, closeDoor_status);
-    }
   }
-}
+
